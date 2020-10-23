ce310b1570db964575367f88e72e0fe56029ccfa
echosoar <82163514@qq.com>
2020-10-23 17:56:23 +0800

    fix: dev pack start auto remove cache

--- packages/faas-dev-pack/src/common.ts
+++ packages/faas-dev-pack/src/common.ts
@@ -1,4 +1,6 @@
 import { invoke } from '@midwayjs/serverless-invoke';
+import { existsSync, remove } from 'fs-extra';
+import { join } from 'path';
 export function resolveModule(gatewayName: string) {
   const gatewayJSON = require('../gateway.json');
   if (gatewayJSON[gatewayName]) {
@@ -10,5 +12,15 @@ export function resolveModule(gatewayName: string) {
 
 export async function invokeFunction(options) {
   options.incremental = options.incremental ?? true;
+  // 首次的时候执行清理
+  if (!process.env.MIDWAT_FIRST_START_TMP_VAR) {
+    const distDir = join(options?.functionDir || process.cwd(), '.faas_debug_tmp');
+    if (existsSync(distDir)) {
+      try {
+        await remove(distDir);
+      } catch {}
+    }
+    process.env.MIDWAT_FIRST_START_TMP_VAR = 'true';
+  }
   return invoke(options);
 }
