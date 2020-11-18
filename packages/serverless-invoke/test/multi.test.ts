818ae861d5e009396a60bee0d8bd0ef71093daa2
Gao Yang <echosoar@qq.com>
2020-11-18 16:22:56 +0800

    fix: multi function invoke same time (#26)
    * fix: multi function invoke same time
    
    * build: lerna npm

--- packages/serverless-invoke/test/multi.test.ts
+++ packages/serverless-invoke/test/multi.test.ts
@@ -12,10 +12,11 @@ describe('/test/multi.test.ts', () => {
       }
     }
   });
-
   it('two at same time', async () => {
+    const MIDWAY_TS_MODE = process.env.MIDWAY_TS_MODE;
+    process.env.MIDWAY_TS_MODE = 'false';
     const result = await Promise.all(
-      ['http', 'http'].map((functionName: string) => {
+      ['http', 'a', 'c', 'http', 'a', 'c'].map((functionName: string) => {
         return invoke({
           functionDir: join(__dirname, 'fixtures/multiApp'),
           functionName,
@@ -34,32 +35,11 @@ describe('/test/multi.test.ts', () => {
         });
       })
     );
-    assert(result.length === 2);
-    const data = JSON.parse(result[0].body);
-    assert(data.method === 'GET' && data.path === '/test/xxx');
-  });
-  it('two at same time', async () => {
-    const result = await Promise.all(
-      ['http', 'a'].map((functionName: string) => {
-        return invoke({
-          functionDir: join(__dirname, 'fixtures/multiApp'),
-          functionName,
-          data: [
-            {
-              headers: { 'Content-Type': 'text/json' },
-              method: 'GET',
-              path: '/test/xxx',
-              query: { name: 123 },
-              body: {
-                name: 'test',
-              },
-            },
-          ],
-          clean: false,
-        });
-      })
-    );
-    assert(result.length === 2);
+    process.env.MIDWAY_TS_MODE = MIDWAY_TS_MODE;
+    assert(result.length === 6);
     assert(result[1].body === 'abc');
+    assert(result[2].body === '123');
+    assert(result[4].body === 'abc');
+    assert(result[5].body === '123');
   });
 });
