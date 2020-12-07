1faae549100effdf7f2c90d423a6de1a4c824b69
echosoar <82163514@qq.com>
2020-12-07 17:25:14 +0800

    style: lint

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -12,9 +12,11 @@ const cliFun = async argv => {
     .then(() => {
       process.exit();
     })
-    .catch((e) => {
+    .catch(e => {
       console.log('\n\n\n');
-      console.log('Error! You can try adding the -V parameter for more information output.');
+      console.log(
+        'Error! You can try adding the -V parameter for more information output.'
+      );
       console.log('\n\n\n');
       console.error(e);
       process.exitCode = 1;
