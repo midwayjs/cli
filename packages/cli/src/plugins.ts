ef8ff421d4be0840a8b7c0289c26fccd1170645f
echosoar <82163514@qq.com>
2021-03-03 21:07:46 +0800

    fix: cli deploy

--- packages/cli/src/plugins.ts
+++ packages/cli/src/plugins.ts
@@ -7,6 +7,7 @@ export const PluginList: Array<{
 }> = [
   { mod: '@midwayjs/cli-plugin-build', command: 'build' },
   { mod: '@midwayjs/cli-plugin-dev', command: 'dev' },
+  { mod: '@midwayjs/cli-plugin-faas', command: 'deploy' },
   { mod: '@midwayjs/cli-plugin-test', command: 'test' },
   { mod: '@midwayjs/cli-plugin-test', command: 'cov' },
   { mod: '@midwayjs/cli-plugin-clean', command: 'clean' },
