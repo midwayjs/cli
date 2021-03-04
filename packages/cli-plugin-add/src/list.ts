51c380ac9e6e8e146db2a8eb0ce0689742db780d
echosoar <82163514@qq.com>
2021-03-04 20:54:40 +0800

    fix: add boilerplate

---
+++ packages/cli-plugin-add/src/list.ts
@@ -0,0 +1,37 @@
+export const templateList = {
+  web: {
+    desc: 'A web application boilerplate with midway',
+    package: '@midwayjs-examples/applicaiton-web',
+    group: 'Web Application',
+  },
+  'web-koa': {
+    desc: 'A web application boilerplate with midway and koa',
+    package: '@midwayjs-examples/applicaiton-koa',
+    group: 'Web Application',
+  },
+  'web-express': {
+    desc: 'A web application boilerplate with midway and express',
+    package: '@midwayjs-examples/applicaiton-express',
+    group: 'Web Application',
+  },
+  grpc: {
+    desc: 'Midway grpc boilerplate',
+    package: '@midwayjs-examples/application-grpc',
+    group: 'Web Application',
+  },
+  serverless: {
+    desc: 'A serverless boilerplate for aliyun fc, tencent scf and so on',
+    package: '@midwayjs/faas-boilerplate-standard',
+    group: 'Serverless',
+  },
+  'serverless-react-with-hooks': {
+    desc: 'A serverless exmaple with react and use hooks',
+    package: '@midwayjs-examples/midway-hooks-react',
+    group: 'Serverless',
+  },
+  'serverless-vue-with-hooks': {
+    desc: 'A serverless exmaple with vue3 and use hooks',
+    package: '@midwayjs-examples/midway-hooks-vue3',
+    group: 'Serverless',
+  },
+};
