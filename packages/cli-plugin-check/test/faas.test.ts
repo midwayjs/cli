15448d5a4174c42bd7d54b3300fa6d40266a2495
soar.gy <soar.gy@alibaba-inc.com>
2022-01-11 14:15:06 +0800

    feat: check deployType

--- packages/cli-plugin-check/test/faas.test.ts
+++ packages/cli-plugin-check/test/faas.test.ts
@@ -2,6 +2,9 @@
 import { runCheck } from './utils';
 import * as assert from 'assert';
 import { join } from 'path';
+import { ensureDir, existsSync, remove } from 'fs-extra';
+import { writeFileSync } from 'fs';
+import * as YAML from 'js-yaml';
 describe('test/faas.test.ts', () => {
   it('check', async () => {
     const cwd = join(__dirname, 'fixtures/faas');
@@ -44,5 +47,52 @@ describe('test/faas.test.ts', () => {
         'there is a duplicate class name(A) in index.ts and index2.ts'
       )
     );
+  });
+
+  it('migrate to faas', async () => {
+    const baseDir = join(__dirname, 'fixtures/migrate-to-faas');
+    const migrateType = [
+      { mod: '@midwayjs/koa', type: 'koa', deployType: '' },
+      { mod: '@midwayjs/koa', type: 'koa', deployType: 'koa' },
+      { mod: 'koa', type: 'koa' },
+      { mod: 'koa', type: 'koa', deployType: 'koa' },
+      { mod: '@midwayjs/express', type: 'express' },
+      { mod: '@midwayjs/express', type: 'express', deployType: 'express' },
+      { mod: 'express', type: 'express' },
+      { mod: 'express', type: 'express', deployType: 'express' },
+      { mod: '@midwayjs/web', type: 'egg' },
+      { mod: '@midwayjs/web', type: 'egg', deployType: 'egg' },
+      { mod: 'egg', type: 'egg' },
+      { mod: 'egg', type: 'egg', deployType: 'egg' },
+    ];
+
+    for (const typeInfo of migrateType) {
+      const testDir = join(baseDir, 'run');
+      if (existsSync(testDir)) {
+        await remove(testDir);
+      }
+      await ensureDir(testDir);
+      writeFileSync(
+        join(testDir, 'package.json'),
+        JSON.stringify({ dependencies: { [typeInfo.mod]: '*' } })
+      );
+      writeFileSync(
+        join(testDir, 'f.yml'),
+        YAML.dump(
+          typeInfo.deployType ? { deployType: typeInfo.deployType } : {}
+        )
+      );
+      const logs = await runCheck(testDir);
+      const logStr = logs.join('\n');
+      if (!typeInfo.deployType) {
+        assert(
+          logStr.includes(
+            `Deploying ${typeInfo.type} as FAAS requires configuring the deployType as ${typeInfo.type} in the f.yml file`
+          )
+        );
+      } else {
+        assert(!logStr.includes(`Deploying ${typeInfo.type} as FAAS`));
+      }
+    }
   });
 });
