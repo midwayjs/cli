3195317ee17aefdce9bb1a9fcc470ef5c3fd35ad
soar.gy <soar.gy@alibaba-inc.com>
2022-01-20 17:17:48 +0800

    fix: postinstall

--- packages/cli-plugin-faas/postinstall.js
+++ packages/cli-plugin-faas/postinstall.js
@@ -1,15 +1,17 @@
-const {
-  postInstall,
-  postInstallModule,
-  formatModuleVersion,
-} = require('@midwayjs/command-core');
-(async () => {
-  const info = await postInstall();
-  const version = formatModuleVersion(
-    info && info.deps['@midwayjs/faas']
-  ).major;
-  if (!version) {
-    return;
-  }
-  await postInstallModule([{ name: '@midwayjs/serverless-app', version }]);
-})();
+try {
+  const {
+    postInstall,
+    postInstallModule,
+    formatModuleVersion,
+  } = require('@midwayjs/command-core');
+  (async () => {
+    const info = await postInstall();
+    const version = formatModuleVersion(
+      info && info.deps['@midwayjs/faas']
+    ).major;
+    if (!version) {
+      return;
+    }
+    await postInstallModule([{ name: '@midwayjs/serverless-app', version }]);
+  })();
+} catch (e) {}
