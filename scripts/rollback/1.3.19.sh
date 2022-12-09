f12f31cb43d90812de7eceeb2c039ead316bb0bf
soar.gy <soar.gy@alibaba-inc.com>
2022-12-09 16:39:21 +0800

    chore: gen rollback to 1.3.19

---
+++ scripts/rollback/1.3.19.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+npm dist-tag add @midwayjs/cli-plugin-add@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-build@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-check@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-clean@1.3.19 latest
+npm dist-tag add @midwayjs/fcli-plugin-deploy@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-dev@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-faas@1.3.19 latest
+npm dist-tag add @midwayjs/fcli-plugin-fc@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-generator@1.3.19 latest
+npm dist-tag add @midwayjs/fcli-plugin-package@1.3.19 latest
+npm dist-tag add @midwayjs/fcli-plugin-scf@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-swc@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-test@1.3.19 latest
+npm dist-tag add @midwayjs/cli-plugin-vercel@1.3.19 latest
+npm dist-tag add @midwayjs/fcli-plugin-wechat@1.3.19 latest
+npm dist-tag add @midwayjs/cli@1.3.19 latest
+npm dist-tag add @midwayjs/command-core@1.3.19 latest
+npm dist-tag add @midwayjs/serverless-dev-pack@1.3.19 latest
+npm dist-tag add @midwayjs/faas-code-analysis@1.3.19 latest
+npm dist-tag add @midwayjs/gateway-common-core@1.3.19 latest
+npm dist-tag add @midwayjs/gateway-common-http@1.3.19 latest
+npm dist-tag add @midwayjs/serverless-meta-json@1.3.19 latest
+npm dist-tag add @midwayjs/serverless-spec-builder@1.3.19 latest
+
+# sync:
+
+cnpm sync @midwayjs/cli-plugin-add
+tnpm sync @midwayjs/cli-plugin-add
+cnpm sync @midwayjs/cli-plugin-build
+tnpm sync @midwayjs/cli-plugin-build
+cnpm sync @midwayjs/cli-plugin-check
+tnpm sync @midwayjs/cli-plugin-check
+cnpm sync @midwayjs/cli-plugin-clean
+tnpm sync @midwayjs/cli-plugin-clean
+cnpm sync @midwayjs/fcli-plugin-deploy
+tnpm sync @midwayjs/fcli-plugin-deploy
+cnpm sync @midwayjs/cli-plugin-dev
+tnpm sync @midwayjs/cli-plugin-dev
+cnpm sync @midwayjs/cli-plugin-faas
+tnpm sync @midwayjs/cli-plugin-faas
+cnpm sync @midwayjs/fcli-plugin-fc
+tnpm sync @midwayjs/fcli-plugin-fc
+cnpm sync @midwayjs/cli-plugin-generator
+tnpm sync @midwayjs/cli-plugin-generator
+cnpm sync @midwayjs/fcli-plugin-package
+tnpm sync @midwayjs/fcli-plugin-package
+cnpm sync @midwayjs/fcli-plugin-scf
+tnpm sync @midwayjs/fcli-plugin-scf
+cnpm sync @midwayjs/cli-plugin-swc
+tnpm sync @midwayjs/cli-plugin-swc
+cnpm sync @midwayjs/cli-plugin-test
+tnpm sync @midwayjs/cli-plugin-test
+cnpm sync @midwayjs/cli-plugin-vercel
+tnpm sync @midwayjs/cli-plugin-vercel
+cnpm sync @midwayjs/fcli-plugin-wechat
+tnpm sync @midwayjs/fcli-plugin-wechat
+cnpm sync @midwayjs/cli
+tnpm sync @midwayjs/cli
+cnpm sync @midwayjs/command-core
+tnpm sync @midwayjs/command-core
+cnpm sync @midwayjs/serverless-dev-pack
+tnpm sync @midwayjs/serverless-dev-pack
+cnpm sync @midwayjs/faas-code-analysis
+tnpm sync @midwayjs/faas-code-analysis
+cnpm sync @midwayjs/gateway-common-core
+tnpm sync @midwayjs/gateway-common-core
+cnpm sync @midwayjs/gateway-common-http
+tnpm sync @midwayjs/gateway-common-http
+cnpm sync @midwayjs/serverless-meta-json
+tnpm sync @midwayjs/serverless-meta-json
+cnpm sync @midwayjs/serverless-spec-builder
+tnpm sync @midwayjs/serverless-spec-builder
